f61f61420905e3e8e055d192117abb143f2c959c
Punam Ahire <punambahire@gmail.com>
2022-06-21 15:13:50 -0500

    Added CRUD functionality for owners and dogs

--- Controllers/DogsController.cs
+++ Controllers/DogsController.cs
@@ -7,113 +7,114 @@ using System.Collections.Generic;
 
 namespace DogGo.Controllers
 {
-    public class OwnersController : Controller
+    public class DogsController : Controller
     {
-        private readonly IOwnerRepository _ownerRepo;
+        private readonly IDogRepository _dogRepo;
 
-        // ASP.NET will give us an instance of our Owner Repository. This is called "Dependency Injection"
-        public OwnersController(IOwnerRepository ownerRepository)
+        // ASP.NET will give us an instance of our Dog Repository. This is called "Dependency Injection"
+        public DogsController(IDogRepository dogRepository)
         {
-            _ownerRepo = ownerRepository;
+            _dogRepo = dogRepository;
         }
 
-        // GET: Owners
+        // GET: Dogs
         public ActionResult Index()
         {
-            List<Owner> owners = _ownerRepo.GetAllOwners();
+            List<Dog> dogs = _dogRepo.GetAllDogs();
 
-            return View(owners);
+            return View(dogs);
         }
 
-        // GET: Owners/Details/5
+        // GET: Dogs/Details/5
         public ActionResult Details(int id)
         {
-            Owner owner = _ownerRepo.GetOwnerById(id);
+            Dog dog = _dogRepo.GetDogById(id);
 
-            if (owner == null)
+            if (dog == null)
             {
                 return NotFound();
             }
 
-            return View(owner);
+            return View(dog);
         }
 
-        // GET: Owners/Create
+        // GET: Dogs/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Owners/Create
+        // POST: Dogs/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Owner owner)
+        public ActionResult Create(Dog dog)
         {
             try
             {
-                _ownerRepo.AddOwner(owner);
+                _dogRepo.AddDog(dog);
 
                 return RedirectToAction("Index");
             }
             catch (Exception ex)
             {
-                return View(owner);
+                return View(dog);
             }
         }
 
-        // GET: Owners/Edit/5
+        // GET: Dogs/Edit/5
         public ActionResult Edit(int id)
         {
-            Owner owner = _ownerRepo.GetOwnerById(id);
+            Dog dog = _dogRepo.GetDogById(id);
 
-            if (owner == null)
+            if (dog == null)
             {
                 return NotFound();
             }
 
-            return View(owner);
+            return View(dog);
         }
 
-        // POST: Owners/Edit/5
+        // POST: Dogs/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, Owner owner)
+        public ActionResult Edit(int id, Dog dog)
         {
             try
             {
-                _ownerRepo.UpdateOwner(owner);
+                _dogRepo.UpdateDog(dog);
 
                 return RedirectToAction("Index");
             }
             catch (Exception ex)
             {
-                return View(owner);
+                return View(dog);
             }
         }
 
-        // GET: Owners/Delete/5
+        // GET: Dogs/Delete/5
         public ActionResult Delete(int id)
         {
-            Owner owner = _ownerRepo.GetOwnerById(id);
+            Dog dog = _dogRepo.GetDogById(id);
 
-            return View(owner);
+            return View(dog);
         }
 
-        // POST: Owners/Delete/5
+        // POST: Dogs/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, Owner owner)
+        public ActionResult Delete(int id, Dog dog)
         {
             try
             {
-                _ownerRepo.DeleteOwner(id);
+                _dogRepo.DeleteDog(id);
 
                 return RedirectToAction("Index");
             }
             catch (Exception ex)
             {
-                return View(owner);
+                return View(dog);
             }
         }
     }
 }
+
---
+++ Repositories/DogRepository.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public class DogRepository : IDogRepository
+    {
+        private readonly IConfiguration _config;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public DogRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Dog> GetAllDogs()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], Breed, OwnerId, Notes, ImageUrl
+                        FROM Dog
+                    ";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Dog> dogs = new List<Dog>();
+                        while (reader.Read())
+                        {
+                            int notesColPosition = reader.GetOrdinal("Notes");
+                            int imageColPosition = reader.GetOrdinal("ImageUrl");
+
+                            Dog dog = new Dog
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                                OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                Notes = reader.IsDBNull(notesColPosition) ? null : (reader.GetString(notesColPosition)),
+                                ImageUrl = reader.IsDBNull(imageColPosition) ? null : (reader.GetString(imageColPosition))  
+                            };
+
+                            dogs.Add(dog);
+                        }
+
+                        return dogs;
+                    }
+                }
+            }
+        }
+
+        public Dog GetDogById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], Breed, OwnerId, Notes, ImageUrl
+                        FROM Dog
+                        WHERE Id = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    List<Dog> dogs = new List<Dog>();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Dog dog = null;
+                        if (reader.Read())
+                        {
+                            int notesColPosition = reader.GetOrdinal("Notes");
+                            int imageColPosition = reader.GetOrdinal("ImageUrl");
+
+                            dog = new Dog
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                                OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                Notes = reader.IsDBNull(notesColPosition) ? null : (reader.GetString(notesColPosition)),
+                                ImageUrl = reader.IsDBNull(imageColPosition) ? null : (reader.GetString(imageColPosition))
+                            };   
+                        }
+                        return dog;
+                    }
+                }
+            }
+        }
+
+
+        public void AddDog(Dog dog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Dog ([Name], Breed, OwnerId, Notes, ImageUrl)
+                    OUTPUT INSERTED.ID
+                    VALUES (@name, @breed, @ownerId, @notes, @imageUrl);    
+                ";
+
+                    cmd.Parameters.AddWithValue("@name", dog.Name);
+                    cmd.Parameters.AddWithValue("@breed", dog.Breed);
+                    cmd.Parameters.AddWithValue("@ownerId", dog.OwnerId);
+                    cmd.Parameters.AddWithValue("@notes", ((object)dog.Notes) ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@imageUrl", ((object)dog.ImageUrl) ?? DBNull.Value);
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    dog.Id = id;
+                }
+            }
+        }
+
+        public void UpdateDog(Dog dog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            UPDATE Dog
+                            SET 
+                                [Name] = @name, 
+                                Breed = @breed, 
+                                OwnerId = @ownerId, 
+                                Notes = @notes, 
+                                ImageUrl = @imageUrl
+                            WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", dog.Id);
+                    cmd.Parameters.AddWithValue("@name", dog.Name);
+                    cmd.Parameters.AddWithValue("@breed", dog.Breed);
+                    cmd.Parameters.AddWithValue("@ownerId", dog.OwnerId);
+                    cmd.Parameters.AddWithValue("@notes", ((object)dog.Notes) ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@imageUrl", ((object)dog.ImageUrl) ?? DBNull.Value);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteDog(int dogId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            DELETE FROM Dog
+                            WHERE Id = @id
+                        ";
+
+                    cmd.Parameters.AddWithValue("@id", dogId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
