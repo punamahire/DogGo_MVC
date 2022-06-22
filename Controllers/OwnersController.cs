21f63e0f6f8cdec6e3ee5e55ddbffd3ad4286761
Punam Ahire <punambahire@gmail.com>
2022-06-22 10:45:33 -0500

    modified Owner's Edit to use ViewModel instead of View

--- Controllers/OwnersController.cs
+++ Controllers/OwnersController.cs
@@ -87,13 +87,15 @@ namespace DogGo.Controllers
         public ActionResult Edit(int id)
         {
             Owner owner = _ownerRepo.GetOwnerById(id);
+            List<Neighborhood> neighborhoods = _neighborhoodRepo.GetAll();
 
-            if (owner == null)
+            OwnerFormViewModel vm = new OwnerFormViewModel()
             {
-                return NotFound();
-            }
+                Owner = owner,
+                Neighborhoods = neighborhoods
+            };
 
-            return View(owner);
+            return View(vm);
         }
 
         // POST: Owners/Edit/5
