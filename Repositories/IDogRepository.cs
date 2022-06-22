fd6bee6b14077949033976297e9410bdcc7ebe29
Punam Ahire <punambahire@gmail.com>
2022-06-22 10:32:50 -0500

    Added ViewModel to display owner, walkers and dogs. Owner Details and Create refactored

--- Repositories/IDogRepository.cs
+++ Repositories/IDogRepository.cs
@@ -11,5 +11,6 @@ namespace DogGo.Repositories
         void AddDog(Dog dog);
         void UpdateDog(Dog dog);
         void DeleteDog(int dogId);
+        List<Dog> GetDogsByOwnerId(int ownerId);
     }
 }
---
+++ Repositories/INeighborhoodRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using DogGo.Models;
+using DogGo.Repositories;
+
+namespace DogGo.Repositories
+{
+    public interface INeighborhoodRepository
+    {
+        List<Neighborhood> GetAll();
+    }
+}
